51b6a6337a96d3586d76c03198919a17c7f5b25d
ncoder-m <userniyaz09@gmail.com>
2026-08-09 21:54:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ async def process_weight(message: Message, state: FSMContext):
         ],
         resize_keyboard=True
     )
-    await message.answer("Шаг 2 из 3:\nУкажите длину вылета консоли (в метрах):", reply_markup=length_kb)
+    await message.answer("Шаг 2 из 3:\nУкажите длину пролета консоли (в метрах):", reply_markup=length_kb)
     
 # ШАГ 3,5: Квиз - Высота пролёта крана
 @dp.message(QuizStates.length)
